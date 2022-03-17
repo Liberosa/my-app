b83caf2ca406d0c67ac47989558fe8aca5000277
d.volchok <d.volchok@itransition.com>
2022-03-17 14:47:47 +0300

    first stilization

--- src/CurrencyInput.jsx
+++ src/CurrencyInput.jsx
@@ -1,36 +1,42 @@
 import PropTypes from "prop-types";
-import { Dropdown, DropdownButton } from "react-bootstrap";
-
+import { Form, Row,Col } from "react-bootstrap";
 
 function CurrencyInput(props) {
-    return (
-        <div>
-            <input type="text" value={props.amount} onChange={event => props.onAmountChange(event.target.value)} />
-            <select value={props.currency} onChange={event => props.onCurrencyChange(event.target.value)}>
-                {props.currencies.map((currency => (
-                    <option value={currency} key={currency}>{currency}</option>
-                )))}
-            </select>
-            
-                <DropdownButton title={props.currency} variant="success" onSelect={event => props.onCurrencyChange(event.target.value)}>
-                {props.currencies.map((currency => (
-                    <Dropdown.Item value={currency} key={currency}>{currency}</Dropdown.Item>
-                )))}
-                
-                </DropdownButton>
-            
+  return (
+    <div>
+      <Form>
+        <Row className="align-items-center">
+          <Col xs="2">
+            <Form.Control
+              className="mb-2"
+              value={props.amount}
+              onChange={(event) => props.onAmountChange(event.target.value)}
+            />
 
-        </div>
-
-    );
+            <Form.Select
+              size="sm"
+              value={props.currency}
+              onChange={(event) => props.onCurrencyChange(event.target.value)}
+            >
+              {props.currencies.map((currency) => (
+                <option value={currency} key={currency}>
+                  {currency}
+                </option>
+              ))}
+            </Form.Select>
+          </Col>
+        </Row>
+      </Form>
+    </div>
+  );
 }
 
 CurrencyInput.propTypes = {
-    amount: PropTypes.number.isRequired,
-    currency: PropTypes.string.isRequired,
-    currencies: PropTypes.array,
-    onAmountChange: PropTypes.func,
-    onCurrencyChange: PropTypes.func,
-}
+  amount: PropTypes.number.isRequired,
+  currency: PropTypes.string.isRequired,
+  currencies: PropTypes.array,
+  onAmountChange: PropTypes.func,
+  onCurrencyChange: PropTypes.func,
+};
 
 export default CurrencyInput;
