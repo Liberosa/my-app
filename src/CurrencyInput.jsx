b06ac7cfe898e30ddd2bc9fa6ae74e4e9f30bde1
d.volchok <d.volchok@itransition.com>
2022-03-18 15:59:25 +0300

    finishedHim!!

--- src/CurrencyInput.jsx
+++ src/CurrencyInput.jsx
@@ -1,32 +1,37 @@
 import PropTypes from "prop-types";
-import { Form, Row,Col } from "react-bootstrap";
+import { Form, Row, Col, Container } from "react-bootstrap";
 
 function CurrencyInput(props) {
   return (
     <div>
-      <Form>
-        <Row className="align-items-center">
-          <Col xs="2">
-            <Form.Control
-              className="mb-2"
-              value={props.amount}
-              onChange={(event) => props.onAmountChange(event.target.value)}
-            />
-
-            <Form.Select
-              size="sm"
-              value={props.currency}
-              onChange={(event) => props.onCurrencyChange(event.target.value)}
-            >
-              {props.currencies.map((currency) => (
-                <option value={currency} key={currency}>
-                  {currency}
-                </option>
-              ))}
-            </Form.Select>
-          </Col>
-        </Row>
-      </Form>
+      <Container>
+        <Form className="m-0">
+          <Row className="align-items-center">
+            <Col xs="auto" className="p-0">
+              <Form.Control
+                className="m-0"
+                size="sm"
+                value={props.amount}
+                onChange={(event) => props.onAmountChange(event.target.value)}
+              />
+               </Col>
+               <Col xs="auto" className="col-xs-3 p-0">
+              <Form.Select
+                className="m-0"
+                size="sm"
+                value={props.currency}
+                onChange={(event) => props.onCurrencyChange(event.target.value)}
+              >
+                {props.currencies.map((currency) => (
+                  <option value={currency} key={currency}>
+                    {currency}
+                  </option>
+                ))}
+              </Form.Select>
+            </Col>
+          </Row>
+        </Form>
+      </Container>
     </div>
   );
 }
